8dd5ee0a1c2bba1a823ab25e5bf4f0fe7381836a
KreskoLab <redeezol@gmail.com>
2022-05-15 22:55:14 +0300

    feat: add default timezone

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
+  process.env.TZ = "Europa/Kiev";
+
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT || 8000);
 }
