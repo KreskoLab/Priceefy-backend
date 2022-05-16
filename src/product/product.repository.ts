46904b5bb5c782abab00db4ec73e2eabcc592941
KreskoLab <redeezol@gmail.com>
2022-05-17 00:02:24 +0300

    style: replace Array with []

--- src/product/product.repository.ts
+++ src/product/product.repository.ts
@@ -2,13 +2,12 @@ import { Injectable } from "@nestjs/common";
 import { Product } from "./schemas/product.schema";
 import { Model } from "mongoose";
 import { InjectModel } from "@nestjs/mongoose";
-import { CreateProductDto } from "./dto/create-product.dto";
 
 @Injectable()
 export class ProductRepository {
   constructor(@InjectModel("Product") private model: Model<Product>) {}
 
-  async create(products: CreateProductDto[]): Promise<void> {
+  async create(products: object[]): Promise<void> {
     await this.model.bulkWrite(products);
   }
 
