86a7206524b4f1ae0fa13107d426e1e63f8384c6
KreskoLab <redeezol@gmail.com>
2022-07-10 19:23:57 +0300

    feat: add api app

---
+++ apps/api/src/api.controller.ts
@@ -0,0 +1,59 @@
+import { CategoriesService } from "@app/categories";
+import { Product, ProductsService, Sort } from "@app/products";
+import { StoresService } from "@app/stores";
+import { City, LocalStore } from "@app/stores";
+import { Controller, Get, Param, Query } from "@nestjs/common";
+
+@Controller()
+export class ApiController {
+  constructor(
+    private readonly storesService: StoresService,
+    private readonly categoriesService: CategoriesService,
+    private readonly productsService: ProductsService,
+  ) {}
+
+  @Get("stores")
+  async getStores(): Promise<Partial<LocalStore>[]> {
+    return this.storesService.getStores();
+  }
+
+  @Get("categories")
+  async getCategories(): Promise<Partial<LocalStore>[]> {
+    return this.categoriesService.getCategories();
+  }
+
+  @Get("cities")
+  async getCities(): Promise<Pick<City, "name" | "slug">[]> {
+    return this.storesService.getCities();
+  }
+
+  @Get("products")
+  async getProducts(
+    @Query("page") page = 1,
+    @Query("sort") sort: Sort = "discount",
+    @Query("city") city: string,
+  ): Promise<Product[]> {
+    return this.productsService.get(page, sort, city);
+  }
+
+  @Get("categories/:slug")
+  async getCategoryProducts(
+    @Param("slug") slug: string,
+    @Query("page") page = 1,
+    @Query("sort") sort: Sort = "discount",
+    @Query("store") store: string,
+    @Query("city") city: string,
+  ): Promise<any> {
+    return this.categoriesService.getProducts(slug, page, sort, store, city);
+  }
+
+  @Get("stores/:slug")
+  async getStoresProducts(
+    @Param("slug") slug: string,
+    @Query("page") page = 1,
+    @Query("sort") sort: Sort = "discount",
+    @Query("city") city: string,
+  ): Promise<any> {
+    return this.storesService.getProducts(slug, page, sort, city);
+  }
+}
