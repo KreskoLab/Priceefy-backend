78e66d95bcb88516660f00e88d7448c6ed2ec9d8
KreskoLab <redeezol@gmail.com>
2022-07-13 00:11:54 +0300

    feat (utils): improve name handler

--- libs/utils/src/utils.service.ts
+++ libs/utils/src/utils.service.ts
@@ -20,50 +20,11 @@ export class UtilsService {
     return Slugify(slug);
   }
 
-  public normalizeName(name: string, category: `${CATEGORIES}`): string {
-    const uselessWords = [
-      "ваговий",
-      "вагове",
-      "вагова",
-      "свіжий",
-      "свіжа",
-      "органічне",
-      "преміум",
-      "елітний",
-      "еліт",
-      "напій",
-      "газований",
-      "негазований",
-      "чай",
-      "холодний",
-      "соковмісний",
-      "сокомісткий",
-      "безалкогольний",
-      "питний",
-      "б/алк",
-      "сил/газ",
-      "добрий",
-      "звичай",
-      "калібр",
-      "сильногазований",
-      "імпорт",
-      "скло",
-      "4х0",
-      "чипси",
-      "чіпси",
-      "снек",
-      "пікантний",
-      "картопляні",
-      "натуральні",
-      "картоп",
-      "пепсі-кола",
-    ];
-
-    const expStr = uselessWords.join("|");
+  public normalizeName(name: string, category: `${CATEGORIES}`, trademark: string): string {
+    const uselessWords: string[] = [];
 
     let normalized = name
       .toLowerCase()
-      .replace(new RegExp(expStr, "gi"), "")
       .replace(".", ",")
       .replace(/\d+(?=[^(]*\))/, "")
       .replace(
@@ -71,20 +32,67 @@ export class UtilsService {
         /(?<![\u0400-\u04ff])(\d+(\,\d+)?кг|\d+шт|\d+(\,\d+)?г|\d+(\,\d+)?л|\d+(\,\d+)?мл|в*|пв|шт|пе|ппл|пп|пє|пе|х|а)(?![\u0400-\u04ff])/g,
         "",
       )
-      .replace(/[~`!@#$^&«»®*()+={}\[\];:\'\"<>\/\\\?-_]/g, "")
+      .replace(/[~!@#$^&«»®*()+={}\[\];:\'\"<>\/\\\?-_]/g, "")
       .replace(/ +/g, " ")
       .trim()
       .replace(/(^,)|(,$)/g, "");
 
     switch (category) {
-      case CATEGORIES.SOFT_DRINKS:
-        normalized = normalized.replace("пепсі-блек", "max");
+      case CATEGORIES.FRUITS:
+      case CATEGORIES.VEGETABLES:
+        uselessWords.push("ваговий", "вагове", "вагова", "свіжий", "свіжа", "органічне", "органічна");
+        break;
+
+      case CATEGORIES.MILK:
+        uselessWords.push("пе", "пп", "органічне");
         break;
 
       case CATEGORIES.CRISPS:
-        normalized = normalized.replace("baked", "запечені");
-        normalized = normalized.replace("гостр", "гострих");
+        uselessWords.push("чипси", "чіпси", "картопляні", "картоп");
+
+        normalized = normalized
+          .replace("baked", "запечені")
+          .replace("гостр", "гострих")
+          .replace(/(?<![\u0400-\u04ff])і(?![\u0400-\u04ff])/g, "та");
+
         break;
+
+      case CATEGORIES.SOFT_DRINKS:
+        uselessWords.push(
+          "напій",
+          "газований",
+          "пепсі-кола",
+          "безалкогольний",
+          "сильногазований",
+          "середньогазований",
+          "соковмісний",
+          "сокомісткий",
+          "скло",
+          "безкалорійний",
+          "з/б",
+          "зб",
+          "жб",
+          "ж/б",
+          "питний",
+          "б/алк",
+          "балк",
+          "сил/газ",
+          "пляшка",
+        );
+
+        normalized = normalized
+          .replace("пепсі-блек", "max")
+          .replace("black", "max")
+          .replace("orange", "апельсин")
+          .replace("негазований", "без газу");
+        break;
+    }
+
+    const expStr = new RegExp("(?<![\u0400-\u04ff])" + uselessWords.join("|") + "(?![\u0400-\u04ff])", "gi");
+    normalized = normalized.replace(expStr, "").replace(/ +/g, " ").trim();
+
+    if (trademark) {
+      normalized = normalized.replace(trademark.toLowerCase(), trademark);
     }
 
     const normalizedName = normalized.charAt(0).toUpperCase() + normalized.slice(1);
@@ -137,25 +145,41 @@ export class UtilsService {
     unit: string,
     category: `${CATEGORIES}`,
   ): { price: number; weight: number; unit: string } {
-    const normalizedWeight = parseFloat(unit.replace(",", ".").replace(/[^0-9.]/g, ""));
-    const normalizedUnit = unit.replace(",", "").replace(/[0-9]/g, "");
+    const extractedWeight = parseFloat(unit.replace(",", ".").replace(/[^0-9.]/g, ""));
+    const extractedUnit = unit.replace(",", "").replace(/[0-9]/g, "");
+
+    let silpoUnit = extractedUnit;
+    let silpoWeight = extractedWeight && extractedWeight > 0 ? extractedWeight : 1;
+    let silpoPrice = 1;
 
     switch (category) {
       case CATEGORIES.FRUITS:
       case CATEGORIES.VEGETABLES:
       case CATEGORIES.MEAT:
-        if (normalizedWeight && normalizedWeight === 100) {
-          const { unit, weight } = this.handleWeightAndUnit(1, "kg");
-          return { price: price * 10, weight, unit };
-        } else {
-          const { unit, weight } = this.handleWeightAndUnit(normalizedWeight ? normalizedWeight : 1, normalizedUnit);
-          return { price, weight, unit };
+        if (extractedUnit === "г" && extractedWeight === 100) {
+          silpoWeight = 1;
+          silpoPrice = 10;
+          silpoUnit = "кг";
+        } else if (extractedUnit === "кг" && extractedWeight < 1) {
+          silpoWeight = extractedWeight * 1000;
+          silpoUnit = "г";
         }
 
-      default:
-        const { unit, weight } = this.handleWeightAndUnit(normalizedWeight ? normalizedWeight : 1, normalizedUnit);
-        return { price, weight, unit };
+        break;
+
+      case CATEGORIES.MILK:
+      case CATEGORIES.SOFT_DRINKS:
+      case CATEGORIES.JUICE:
+        if (extractedUnit === "л") {
+          silpoWeight = extractedWeight * 1000;
+        }
+
+        silpoUnit = "мл";
+        break;
     }
+
+    const result = this.handleWeightAndUnit(silpoWeight, silpoUnit);
+    return { price: price * silpoPrice, weight: result.weight, unit: result.unit };
   }
 
   public handleZakazWeight(
@@ -167,12 +191,12 @@ export class UtilsService {
     category: `${CATEGORIES}`,
     title: string,
   ) {
-    const extraced = title
+    const extracted = title
       .split(" ")
       .pop()
       .replace(/[`~!@#$%^&*()_|+\-=?;:]/gi, "");
 
-    const extractedWeight = extraced.match(/\d+/) ? Number(extraced.match(/\d+/)[0]) : "";
+    const extractedWeight = parseFloat(extracted.replace(",", ".").replace(/[^0-9.]/g, ""));
 
     switch (category) {
       case CATEGORIES.MILK:
@@ -218,18 +242,27 @@ export class UtilsService {
       case CATEGORIES.CRISPS:
       case CATEGORIES.CROISANT:
       case CATEGORIES.MARSHMALLOW:
-        if (typeof extractedWeight === "number") {
+        if (typeof extractedWeight === "number" && extractedWeight > 0) {
           if (extractedWeight > 5) return this.handleWeightAndUnit(extractedWeight, "г");
           else return this.handleWeightAndUnit(extractedWeight, "кг");
         } else {
-          if (weight > 5) return this.handleWeightAndUnit(weight, "г");
-          else return this.handleWeightAndUnit(weight, "кг");
+          if (unit === "kg") {
+            return this.handleWeightAndUnit(bundle, "кг");
+          }
+
+          if (unit === "pcs") {
+            return this.handleWeightAndUnit(weight, "г");
+          }
         }
 
       case CATEGORIES.MEAT:
-        if (typeof extractedWeight === "number") {
+        if (typeof extractedWeight === "number" && extractedWeight > 0) {
           if (extractedWeight > 5) return this.handleWeightAndUnit(extractedWeight, "г");
-          else return this.handleWeightAndUnit(extractedWeight, "кг");
+          else {
+            if (extractedWeight < 1) {
+              return this.handleWeightAndUnit(extractedWeight * 100, "г");
+            } else return this.handleWeightAndUnit(extractedWeight, "кг");
+          }
         } else {
           if (unit === "kg" && weight === 0) {
             return this.handleWeightAndUnit(bundle, "кг");
