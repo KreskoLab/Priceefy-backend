711b0030940b190194dadd83bf292f33130e45a1
KreskoLab <redeezol@gmail.com>
2022-07-13 22:42:52 +0300

    feat (product): add product prices aggregation

--- libs/products/src/products.repository.ts
+++ libs/products/src/products.repository.ts
@@ -4,6 +4,8 @@ import { Model } from "mongoose";
 import { InjectModel } from "@nestjs/mongoose";
 import { Sort } from "./types/sort";
 import { AggregationResults } from "./interfaces/aggregation-results.interface";
+import { Period } from "./types/period";
+import { Price } from "./interfaces/price.interface";
 
 @Injectable()
 export class ProductsRepository {
@@ -15,6 +17,111 @@ export class ProductsRepository {
 
   async findBySlugs(slugs: string[]): Promise<Pick<Product, "_id" | "category" | "slug">[]> {
     return this.model.find({ slug: { $in: slugs } }, { _id: 1, category: 1, slug: 1 });
+  }
+
+  async aggregatePrices(
+    slug: string,
+    city: string,
+    period: Period,
+  ): Promise<Pick<Price, "price" | "store" | "created_at">[]> {
+    return this.model.aggregate([
+      {
+        $match: {
+          slug: slug,
+        },
+      },
+      {
+        $unwind: "$prices",
+      },
+      {
+        $match: {
+          "prices.city": city,
+        },
+      },
+      {
+        $sort: { "prices.created_at": -1 },
+      },
+      {
+        $group: {
+          _id: "$name",
+          prices: { $addToSet: "$prices" },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          prices: {
+            $map: {
+              input: {
+                $filter: {
+                  input: "$prices",
+                  as: "price",
+                  cond: {
+                    $eq: [
+                      { $dateTrunc: { date: "$$NOW", unit: period, timezone: "Europe/Kiev" } },
+                      { $dateTrunc: { date: "$$price.created_at", unit: period, timezone: "Europe/Kiev" } },
+                    ],
+                  },
+                },
+              },
+              as: "price",
+              in: {
+                price: "$$price.price",
+                store: "$$price.store",
+                created_at: "$$price.created_at",
+              },
+            },
+          },
+        },
+      },
+      {
+        $unwind: "$prices",
+      },
+      {
+        $replaceRoot: { newRoot: "$prices" },
+      },
+    ]);
+  }
+
+  async findBySlug(slug: string, city?: string): Promise<Product[]> {
+    const cityMatch = city ? city : { $exists: true };
+
+    return this.model.aggregate([
+      {
+        $match: {
+          slug: slug,
+        },
+      },
+      {
+        $unwind: "$prices",
+      },
+      {
+        $match: {
+          "prices.city": cityMatch,
+        },
+      },
+      {
+        $sort: { "prices.created_at": -1 },
+      },
+      {
+        $group: {
+          _id: { name: "$name", weight: "$weight", unit: "$unit" },
+          prices: { $addToSet: "$prices" },
+          slug: { $first: "$slug" },
+          image: { $first: "$image" },
+          country: { $first: "$country" },
+          trademark: { $first: "$trademark" },
+          unit: { $first: "$unit" },
+          weight: { $first: "$weight" },
+        },
+      },
+      {
+        $set: {
+          name: "$_id.name",
+          _id: "$$REMOVE",
+        },
+      },
+    ]);
   }
 
   async findAll(page: number, sort: Sort, city?: string): Promise<AggregationResults[]> {
--- libs/products/src/products.service.ts
+++ libs/products/src/products.service.ts
@@ -1,13 +1,17 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { Product } from "./schemas/products.schema";
 import { CreateProductDto } from "./dto/create-product.dto";
 import { ProductsRepository } from "./products.repository";
 import { Sort } from "./types/sort";
 import { AggregationResults } from "./interfaces/aggregation-results.interface";
+import { Period } from "./types/period";
+import { Price } from "./interfaces/price.interface";
+import { UtilsService } from "@app/utils";
+import { LocalStore } from "@app/stores";
 
 @Injectable()
 export class ProductsService {
-  constructor(private readonly productsRepository: ProductsRepository) {}
+  constructor(private readonly productsRepository: ProductsRepository, private readonly utilsService: UtilsService) {}
 
   async create(products: CreateProductDto[]): Promise<void> {
     const bulkDocs = [];
@@ -34,8 +38,27 @@ export class ProductsService {
     return this.productsRepository.findBySlugs(slugs);
   }
 
-  async get(page: number, sort: Sort, city?: string): Promise<AggregationResults> {
+  async getAll(page: number, sort: Sort, city?: string): Promise<AggregationResults> {
     const res = await this.productsRepository.findAll(page, sort, city);
     return res[0];
+  }
+
+  async getBySlug(slug: string, city?: string): Promise<Product> {
+    const res = await this.productsRepository.findBySlug(slug, city);
+    if (res.length) return res[0];
+    else throw new HttpException("Product not found", HttpStatus.NOT_FOUND);
+  }
+
+  async getPrices(
+    slug: string,
+    city: string,
+    period: Period,
+  ): Promise<Pick<Price, "price" | "store" | "created_at">[]> {
+    const stores = await this.utilsService
+      .readData<LocalStore[]>("stores.json")
+      .then((res) => res.map((store) => ({ name: store.name, slug: store.slug })));
+    const res = await this.productsRepository.aggregatePrices(slug, city, period);
+
+    return res.map((item) => ({ ...item, store: stores.find((store) => store.slug === item.store).name }));
   }
 }
