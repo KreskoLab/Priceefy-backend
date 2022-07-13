ae38344a44f45bbae96bbd3efaaaff6492e0f9ba
KreskoLab <redeezol@gmail.com>
2022-07-13 22:42:03 +0300

    feat (product): connect Utils module

--- libs/products/src/products.module.ts
+++ libs/products/src/products.module.ts
@@ -3,9 +3,10 @@ import { ProductsService } from "./products.service";
 import { MongooseModule } from "@nestjs/mongoose";
 import { Product, ProductSchema } from "./schemas/products.schema";
 import { ProductsRepository } from "./products.repository";
+import { UtilsModule } from "@app/utils";
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }])],
+  imports: [MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]), UtilsModule],
   providers: [ProductsService, ProductsRepository],
   exports: [ProductsService],
 })
