304a7f5ff6fdefbe4d9f8e0a15d86f09854cc421
KreskoLab <redeezol@gmail.com>
2022-07-18 19:31:22 +0300

    feat (api): load cors origin from env

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,11 +1,13 @@
+import { ConfigService } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
 import { ApiModule } from "./api.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiModule);
+  const configService = app.get<ConfigService>(ConfigService);
 
   app.enableCors({
-    origin: ["http://localhost:3000"],
+    origin: configService.get<string>("FRONTEND_URL"),
     credentials: true,
   });
 
